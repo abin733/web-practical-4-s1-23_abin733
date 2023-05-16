66b68900a8a0b9aa6266e7c00b9b9db8d9c3bb3d
abin733 <abin733@aucklanduni.ac.nz>
2023-05-17 11:49:45 +1200

    20230517

--- question01/modules/candidate-dao.js
+++ question01/modules/candidate-dao.js
@@ -1,7 +1,21 @@
 const SQL = require("sql-template-strings");
 const dbPromise = require("./database.js");
 
+async function getSkillsByName(name){
+
+    console.log("call getSkillsByName fucntion in animals-dao.js");
+    const db = await dbPromise;
+
+    const skillsArray = await db.all(SQL`
+        SELECT * FROM ANIMAL_DATA
+            WHERE skill = ${name}
+    `)
+    console.log(JSON.stringify(skillsArray));
+    return skillsArray;
+}
+
 // Export functions.
 module.exports = {
-
+    getSkillsByName
 };
+
--- question01/routes/application-routes.js
+++ question01/routes/application-routes.js
@@ -8,6 +8,16 @@ router.get("/", async function(req, res) {
     res.render("home");
 });
 
+router.get("/getskill", async function(req,res){
+    console.log("Get skill call init");
+    const skillName = req.query.skill;
+    console.log("Git:" + animalName);
+    // TODO: call data access function with animalName as paraemeter and return that array of matching animal objects
+    const skillsArray = await candidateDao.
+    console.log("Animal data:" + JSON.stringify(animalsArray));
+    res.locals.animals = animalsArray;
+    res.render("animals");
 
+});
 
 module.exports = router;
